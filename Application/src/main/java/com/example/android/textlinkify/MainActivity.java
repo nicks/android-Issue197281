69b636189acf5da4404e9c488e1ea48afd21f3a8
Nick Santos <nick@medium.com>
2015-12-17 10:16:51 -0800

    Add repro steps for https://code.google.com/p/android/issues/detail?id=197281

--- Application/src/main/java/com/example/android/textlinkify/MainActivity.java
+++ Application/src/main/java/com/example/android/textlinkify/MainActivity.java
@@ -17,34 +17,21 @@
 package com.example.android.textlinkify;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
+import android.text.style.LineBackgroundSpan;
 import android.text.style.StyleSpan;
 import android.text.style.URLSpan;
 import android.widget.TextView;
 
 /**
- * This sample demonstrates how clickable links can be added to a
- * {@link android.widget.TextView}.
- *
- * <p>This can be done in three ways:
- * <ul>
- * <li><b>Automatically:</b> Text added to a TextView can automatically be linkified by enabling
- * autoLinking. In XML, use the android:autoLink property, programatically call
- * {@link android.widget.TextView#setAutoLinkMask(int)} using an option from
- * {@link android.text.util.Linkify}</li>
- *
- * <li><b>Parsing a String as HTML:</b> See {@link android.text.Html#fromHtml(String)})</li>
- *
- * <li><b>Manually by constructing a {@link android.text.SpannableString}:</b> Consisting of
- * {@link android.text.style.StyleSpan} and {@link android.text.style.URLSpan} objects that
- * are contained within a {@link android.text.SpannableString}</li>
- * </ul></p>
- *
+ * This sample demonstrates https://code.google.com/p/android/issues/detail?id=197281
  */
 public class MainActivity extends Activity {
 
@@ -54,85 +41,28 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.sample_main);
 
-        // BEGIN_INCLUDE(text_auto_linkify)
-        /*
-         *  text_auto_linkify shows the android:autoLink property, which
-         *  automatically linkifies things like URLs and phone numbers
-         *  found in the text. No java code is needed to make this
-         *  work.
-         *  This can also be enabled programmatically by calling
-         *  .setAutoLinkMask(Linkify.ALL) before the text is set on the TextView.
-         *
-         *  See android.text.util.Linkify for other options, for example only
-         *  auto-linking email addresses or phone numbers
-         */
-        // END_INCLUDE(text_auto_linkify)
+        TextView textViewIntro = (TextView) findViewById(R.id.text_intro);
+        textViewIntro.setMovementMethod(LinkMovementMethod.getInstance());
 
-        // BEGIN_INCLUDE(text_html_resource)
-        /*
-         * text_html_resource has links specified by putting anchor tags (<a>) in the string
-         * resource. By default these links will appear but not
-         * respond to user input. To make them active, you need to
-         * call setMovementMethod() on the TextView object.
-         */
-        TextView textViewResource = (TextView) findViewById(R.id.text_html_resource);
-        textViewResource.setText(
-                Html.fromHtml(getResources().getString(R.string.link_text_manual)));
-        textViewResource.setMovementMethod(LinkMovementMethod.getInstance());
-        // END_INCLUDE(text_html_resource)
-
-        // BEGIN_INCLUDE(text_html_program)
-        /*
-         * text_html_program shows creating text with links from HTML in the Java
-         * code, rather than from a string resource. Note that for a
-         * fixed string, using a (localizable) resource as shown above
-         * is usually a better way to go; this example is intended to
-         * illustrate how you might display text that came from a
-         * dynamic source (eg, the network).
-         */
-        TextView textViewHtml = (TextView) findViewById(R.id.text_html_program);
-        textViewHtml.setText(
-                Html.fromHtml(
-                        "<b>text_html_program: Constructed from HTML programmatically.</b>"
-                                + "  Text with a <a href=\"http://www.google.com\">link</a> "
-                                + "created in the Java source code using HTML."));
-        textViewHtml.setMovementMethod(LinkMovementMethod.getInstance());
-        // END_INCLUDE(text_html_program)
-
-        // BEGIN_INCLUDE(text_spannable)
-        /*
-         * text_spannable illustrates constructing a styled string containing a
-         * link without using HTML at all. Again, for a fixed string
-         * you should probably be using a string resource, not a
-         * hardcoded value.
-         */
         SpannableString ss = new SpannableString(
-                "text_spannable: Manually created spans. Click here to dial the phone.");
-
-        /*
-         * Make the first 38 characters bold by applying a StyleSpan with bold typeface.
-         *
-         * Characters 45 to 49 (the word "here") is made clickable by applying a URLSpan
-         * pointing to a telephone number. Clicking it opens the "tel:" URL that starts the dialer.
-         *
-         * The SPAN_EXCLUSIVE_EXCLUSIVE flag defines this span as exclusive, which means
-         * that it will not expand to include text inserted on either side of this span.
-         */
-        ss.setSpan(new StyleSpan(Typeface.BOLD), 0, 39,
+            "Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.");
+        int startIndex = ss.toString().indexOf("dolor");
+        int endIndex = ss.toString().indexOf("Aenean");
+        ss.setSpan(new GreenSpan(), startIndex, endIndex,
                 Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-        ss.setSpan(new URLSpan("tel:4155551212"), 40 + 6, 40 + 10,
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         TextView textViewSpan = (TextView) findViewById(R.id.text_spannable);
         textViewSpan.setText(ss);
+    }
 
-        /*
-         * Set the movement method to move between links in this TextView.
-         * This means that the user traverses through links in this TextView, automatically
-         * handling appropriate scrolling and key commands.
-         */
-        textViewSpan.setMovementMethod(LinkMovementMethod.getInstance());
-        // END_INCLUDE(text_spannable)
+    public class GreenSpan implements LineBackgroundSpan {
+        @Override public void drawBackground(
+            Canvas c, Paint p, int left, int right, int top, int baseline, int bottom, CharSequence text, int start, int end, int lnum) {
+            int prevColor = p.getColor();
+            p.setColor(getApplication().getResources().getColor(android.R.color.holo_green_light));
+            c.drawRect(left, top, right, bottom, p);
+            p.setColor(prevColor);
+        }
     }
 
 }
--- README.md
+++ README.md
@@ -2,62 +2,13 @@
 Android TextLinkify Sample
 ===================================
 
-This sample illustrates how links can be added to a TextView. This can be done either
-automatically by setting the "autoLink" property or explicitly.
+This sample is a demo of https://code.google.com/p/android/issues/detail?id=197281
 
-Introduction
-------------
+LineBackgroundSpan not called on first line of span in multi-line TextView\n
 
-This sample demonstrates how clickable links can be added to a `TextView` in Android.
+The expected behavior is a green background from the line with "dolor" to the line with "Aenean"
 
-This can be done in three ways:
-
-- **Automatically:** Text added to a TextView can automatically be linkified by enabling
-autoLinking. In XML, use the `android:autoLink` property, programatically call
-[android.widget.TextView#setAutoLinkMask(int)][1] using an option from
-[android.text.util.Linkify][2].
-- **Parsing a String as HTML:** See [android.text.Html#fromHtml(String)][3].
-- **Manually by constructing a [android.text.SpannableString][4]:** Consisting of
-[android.text.style.StyleSpan][5] and [android.text.style.URLSpan][6] objects that
-are contained within a [android.text.SpannableString][7].
-
-[1]: http://developer.android.com/reference/android/widget/TextView.html#setAutoLinkMask(int)
-[2]: http://developer.android.com/reference/android/text/util/Linkify.html
-[3]: http://developer.android.com/reference/android/text/Html.html#fromHtml(java.lang.String)
-[4]: http://developer.android.com/reference/android/text/SpannableString.html
-[5]: http://developer.android.com/reference/android/text/style/StyleSpan.html
-[6]: http://developer.android.com/reference/android/text/style/URLSpan.html
-[7]: http://developer.android.com/reference/android/text/SpannableString.html
-
-Pre-requisites
---------------
-
-- Android SDK v23
-- Android Build Tools v23.0.2
-- Android Support Repository
-
-Screenshots
--------------
-
-<img src="screenshots/main.png" height="400" alt="Screenshot"/> 
-
-Getting Started
----------------
-
-This sample uses the Gradle build system. To build this project, use the
-"gradlew build" command or use "Import Project" in Android Studio.
-
-Support
--------
-
-- Google+ Community: https://plus.google.com/communities/105153134372062985968
-- Stack Overflow: http://stackoverflow.com/questions/tagged/android
-
-If you've found an error in this sample, please file an issue:
-https://github.com/googlesamples/android-TextLinkify
-
-Patches are encouraged, and may be submitted by forking this project and
-submitting a pull request through GitHub. Please see CONTRIBUTING.md for more details.
+Adapted from https://github.com/googlesamples/android-TextLinkify
 
 License
 -------
